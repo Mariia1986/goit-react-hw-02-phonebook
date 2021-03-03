06dba312e8ff10ba754fe3bb840e4b25f4ddf259
Mariia Randarevych <renij1986@gmail.com>
2021-03-03 18:02:33 +0300

    добавлен функционал по добавлению имени в телефонную книгу

--- src/App.js
+++ src/App.js
@@ -1,29 +1,59 @@
 import React, { Component } from "react";
+import { v4 as uuidv4 } from 'uuid'
 import "./App.css";
 
 class App extends Component {
   state = {
     contacts: [],
-    name: ''
+    name: "",
+  };
+
+
+  handleInput=(e)=>{
+    this.setState(
+      // console.dir(e.target)
+      { name:e.target.value}
+     )}
+  
+  onHandleSubmit = (e) => {
+    e.preventDefault();
+    this.setState(prevState =>( {
+      contacts:
+      [ ...prevState.contacts, this.state.name]}
+      ));
+    
+      this.resetForm()
+      
+    
   }
- 
+   
+//  resetForm=()=>{
+//  this.setState({
+//   event.target.elements.name.value=''
+//  }
+//  )}
+    
+  
 
   render() {
-  
+    const { name, contacts } = this.state;
+
     return (
       <div className="App">
         <h1>Phonebook</h1>
-        <form>
-          <label>Name
-            <input name="name" type="text"/>
+        <form onSubmit={this.onHandleSubmit}>
+          <label>
+            Name
+            <input name="name" onChange={this.handleInput} type="text" />
           </label>
           <button type="submit">Add contact</button>
         </form>
         <div>
           <h2>Contacts</h2>
-           <ul>
-             
-           </ul>
+          <ul>
+            {contacts.map(el=>
+            <li key={uuidv4()}>{el}</li>)}
+          </ul>
         </div>
       </div>
     );
